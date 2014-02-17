4ee5e2074331d8725aaeb0fa4af549f2e2b918b0
Owner <Owner@Owner-158543>
2014-02-17 13:53:42 -0600

    testing CRUD operations

--- src/Model/DBAccessor.java
+++ src/Model/DBAccessor.java
@@ -1,5 +1,3 @@
-
-
 package Model;
 
 import java.sql.SQLException;
@@ -7,16 +5,28 @@ import java.util.List;
 
 /**
  * This is the contract for all low level JDBC classes.
+ *
  * @author Teresa Mahoney
  */
 public interface DBAccessor {
+
     // open connection
-    public abstract void openConnection(String driverClassName, String url, String username, String password) 
-	throws IllegalArgumentException, ClassNotFoundException, SQLException;
+    public abstract void openConnection(String driverClassName, String url, String username, String password)
+            throws IllegalArgumentException, ClassNotFoundException, SQLException;
+
     //close connection
     public abstract void closeConnection() throws SQLException;
+
 // retrieve records
     public abstract List retrieveRecords(String sqlString, boolean closeConnection) throws SQLException,
-			Exception;
+            Exception;
+    // delete records
+
+    public abstract int deleteRecords(String sqlString, boolean closeConnection) throws SQLException,
+            Exception;
+    public abstract boolean insertRecord(String tableName, List colDescriptors, List colValues, boolean closeConnection)
+	throws SQLException, Exception;
+    public int updateRecords(String tableName, List colDescriptors, List colValues,
+			 String whereField, Object whereValue, boolean closeConnection)
+			 throws SQLException, Exception;
 }
-//
